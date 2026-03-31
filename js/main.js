d3499e1bd07556bd645ff6f10739dda0ebf34665
Rodrigo Arce C <rodrigoar87@gmail.com>
2026-03-31 13:32:24 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// main.js
+function saludar() {
+    alert('¡Hola! Bienvenido a DHB Robótica 🚀');
+}
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
